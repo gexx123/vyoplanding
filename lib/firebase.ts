7195f8c1fe0a5b18fb31504ca60a2245c0c45186
gexx123 <tunwalhimanshu@gmail.com>
2026-04-24 23:39:39 +0530

    secured firebase keys in .env

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDawGPx6rTeAiQ0tEFCCPRMafVQTSEWTh0",
-  authDomain: "paperbot-rhxui0.firebaseapp.com",
-  databaseURL: "https://paperbot-rhxui0-default-rtdb.firebaseio.com",
-  projectId: "paperbot-rhxui0",
-  storageBucket: "paperbot-rhxui0.appspot.com",
-  messagingSenderId: "66257255126",
-  appId: "1:66257255126:web:46b7d07229b02b7328e777"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
